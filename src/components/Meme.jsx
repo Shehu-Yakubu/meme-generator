e6b9669e6ca915c1456facd20af59a4fc7eed5e3
Shehu-Yakubu <shehyak@gmail.com>
2023-03-09 14:26:46 +0100

    Add a Meme component

---
+++ src/components/Meme.jsx
@@ -0,0 +1,13 @@
+function Meme() {
+    return(
+        <form className="form">
+            <div className="form--inputs">
+                <input type="text" />
+                <input type="text" />
+            </div>
+            <button className="form--button">Get a new meme image</button>
+        </form>
+    )
+}
+
+export default Meme
